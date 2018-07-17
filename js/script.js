d311a2afbd53fdeecf44dd36b72877cb7f1c5cfa
Jenny Chiou <01xa14@ms.mingdao.edu.tw>
2018-07-17 16:12:52 +0800

    add files

---
+++ js/script.js
@@ -0,0 +1,132 @@
+$('#start').on('click', function () {
+    if ($('#bigcircle').css('display') == 'none') {
+        $('#bigcircle').show(800);
+    } else {
+        $('#bigcircle').hide(800);
+    }
+    if ($('#smallcircle').css('display') == 'none') {
+        $('#smallcircle').show(800);
+    } else {
+        $('#smallcircle').hide(800);
+    }
+    if ($('#title').css('display') == 'none') {
+        $('#title').show(800);
+    } else {
+        $('#title').hide(800);
+    }
+    if ($('#text1').css('display') == 'none') {
+        $('#text1').show(800);
+    } else {
+        $('#text1').hide(800);
+    }
+    if ($('#text2').css('display') == 'none') {
+        $('#text2').show(800);
+    } else {
+        $('#text2').hide(800);
+    }
+    if ($('#text3').css('display') == 'none') {
+        $('#text3').show(800);
+    } else {
+        $('#text3').hide(800);
+    }
+    if ($('#start').css('display') == 'none') {
+        $('#start').show(800);
+    } else {
+        $('#start').hide(800);
+    }
+    if ($('#redpolygon').css('display') == 'none') {
+        $('#redpolygon').show(800);
+    } else {
+        $('#redpolygon').hide(800);
+    }
+    if ($('#yellowcircle').css('display') == 'none') {
+        $('#yellowcircle').show(800);
+    } else {
+        $('#yellowcircle').hide(800);
+    }
+    if ($('#bluetriangle').css('display') == 'none') {
+        $('#bluetriangle').show(800);
+    } else {
+        $('#bluetriangle').hide(800);
+    }
+    if ($('#battery').css('display') == 'none') {
+        $('#battery').show(800);
+    } else {
+        $('#battery').hide(800);
+    }
+    if ($('#R').css('display') == 'none') {
+        $('#R').show(800);
+    } else {
+        $('#R').hide(800);
+    }
+    if ($('#bnzmark').css('display') == 'none') {
+        $('#bnzmark').show(800);
+    } else {
+        $('#bnzmark').hide(800);
+    }
+    if ($('#bnzmarkdash').css('display') == 'none') {
+        $('#bnzmarkdash').show(800);
+    } else {
+        $('#bnzmarkdash').hide(800);
+    }
+    if ($('#bnzarc').css('display') == 'none') {
+        $('#bnzarc').show(800);
+    } else {
+        $('#bnzarc').hide(800);
+    }
+    if ($('#shoter').css('display') == 'none') {
+        $('#shoter').show(800);
+    } else {
+        $('#shoter').hide(800);
+    }
+    if ($('#yellowshot').css('display') == 'none') {
+        $('#yellowshot').show(800);
+    } else {
+        $('#yellowshot').hide(800);
+    }
+    if ($('#redshot').css('display') == 'none') {
+        $('#redshot').show(800);
+    } else {
+        $('#redshot').hide(800);
+    }
+    if ($('#blueshot').css('display') == 'none') {
+        $('#blueshot').show(800);
+    } else {
+        $('#blueshot').hide(800);
+    }
+    if ($('#save').css('display') == 'none') {
+        $('#save').show(800);
+    } else {
+        $('#save').hide(800);
+    }
+    if ($('#card').css('display') == 'none') {
+        $('#card').show(800);
+    } else {
+        $('#card').hide(800);
+    }
+    if ($('#heart').css('display') == 'none') {
+        $('#heart').show(800);
+    } else {
+        $('#heart').hide(800);
+    }
+    if ($('#time').css('display') == 'none') {
+        $('#time').show(800);
+    } else {
+        $('#time').hide(800);
+    }
+    if ($('#battery2').css('display') == 'none') {
+        $('#battery2').show(800);
+    } else {
+        $('#battery2').hide(800);
+    }
+    if ($('#topleftlogo').css('display') == 'none') {
+        $('#topleftlogo').show(800);
+    } else {
+        $('#topleftlogo').hide(800);
+    }
+    if ($('#sp').css('display') == 'none') {
+        $('#sp').show(800);
+    } else {
+        $('#sp').hide(800);
+    }
+});
